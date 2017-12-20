14885058b9c9dc0d4b221f014a7eec81803de5a9
sungbk <seongsee@gmail.com>
2017-12-20 22:45:14 +0900

    멀티스레드예제 파일에 멀티스레드 기능 구현

--- posix_multithread/posix_multithread.c
+++ posix_multithread/posix_multithread.c
@@ -3,12 +3,17 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define	THREAD_COUNT	3
+
 void *thread_function(void *arg)
 {
 	int i;
+	int me;
+
+	me = *((int *)arg);
 
 	for(i=0;i<20;i++) {
-		printf("Thread running i is \'%d\'\n", i);
+		printf("%d Thread running i is \'%d\'\n",me, i);
 		sleep(1);
 	}
 
@@ -17,16 +22,25 @@ void *thread_function(void *arg)
 
 int main(void)
 {
-	pthread_t mythread;
+	pthread_t mythread[THREAD_COUNT];
+	int thread_num[THREAD_COUNT];
+	int i;
 
-	if (pthread_create(&mythread, NULL, thread_function, NULL)) {
-		printf(" error creating thread.\n");
-		return -1;
+	/* Create Threads */
+	for(i=0;i<THREAD_COUNT;i++) {
+		thread_num[i] = i+1;
+		if (pthread_create(&mythread[i], NULL, thread_function, (void*)&thread_num[i])) {
+			printf("error creating %d's thread.\n", i+1);
+			return -1;
+		}
 	}
 
-	if (pthread_join(mythread, NULL)) {
-		printf("error joining thread.\n");
-		return -1;
+
+	for(i=0;i<THREAD_COUNT;i++) {
+		if (pthread_join(mythread[0], NULL)) {
+			printf("error joining %d's thread.\n", i+1);
+			return -1;
+		}
 	}
 
 	return 0;
